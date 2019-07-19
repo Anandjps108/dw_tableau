fd5405d5ecf0402d24831d3e7a3dac1699a78056
Anandjps108 <46988767+Anandjps108@users.noreply.github.com>
2019-07-19 19:12:26 +0530

    Footfall

--- outpatientfootfall.js
+++ outpatientfootfall.js
@@ -19,7 +19,7 @@
     });
 
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://localhost:8080/thirdparty/reportingservice/reportingservice/patientreports/getoutpatientfootfall/2016-01-01/2016-10-01", function(resp) {
+        $.getJSON("https://demo.incarnus.com:8850/thirdparty/reportingservice/reportingservice/patientreports/getoutpatientfootfall/1000/6/2000-01-01", function(resp) {
             var data = resp.outpatientfootfall,
                 tableData = [];
     
