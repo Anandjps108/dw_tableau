6e36d14a8b7989bc5cf86838d5ef4484748e5d66
Anandjps108 <46988767+Anandjps108@users.noreply.github.com>
2019-07-19 18:27:04 +0530

    Update opdvisittrend.js

--- opdvisittrend.js
+++ opdvisittrend.js
@@ -19,7 +19,7 @@
     });
 
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://demo.incarnus.com:8850/thirdparty/reportingservice/reportingservice/patientreports/getopdvisittrend/1000/6/2000-01-01, function(resp) {
+        $.getJSON("https://localhost:8080/thirdparty/reportingservice/reportingservice/patientreports/getopdvisittrend/1000/6/2000-01-01, function(resp) {
             var data = resp.opdvisittrend,
                 tableData = [];
     
