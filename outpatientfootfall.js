87f7f18d02639a665545720bbcb557c60c4c0e0b
Anandjps108 <46988767+Anandjps108@users.noreply.github.com>
2019-07-19 19:17:30 +0530

    Change

--- outpatientfootfall.js
+++ outpatientfootfall.js
@@ -1,136 +1,138 @@
 (function () {
     var myConnector = tableau.makeConnector();
-
-    myConnector.getSchema = function (schemaCallback) {
-
-    };
-
-    myConnector.getData = function (table, doneCallback) {
-
-    };
-
-    tableau.registerConnector(myConnector);
+    const limit = 10000;
+    var pagenumber = 1;
+    tableData = [];
 
     $(document).ready(function () {
         $("#submitButton").click(function () {
-            tableau.connectionName = "outPatient Footfall";
-            tableau.submit();
+                tableau.connectionName = "outpatientfootfall List";
+                tableau.submit();
         });
     });
 
-    myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://demo.incarnus.com:8850/thirdparty/reportingservice/reportingservice/patientreports/getoutpatientfootfall/1000/6/2000-01-01", function(resp) {
-            var data = resp.outpatientfootfall,
-                tableData = [];
-    
+    myConnector.getData = function (table, doneCallback) {
+        var modifiedat = table.incrementValue
+
+        if (!modifiedat) {
+            modifiedat = "2000-01-01"
+        }
+        else {
+            console.log("modifiedat: " + modifiedat);
+        }
+
+        var queryPath = "https://demo.incarnus.com:8850/thirdparty/tableauservice/patientreports/getoutpatientfootfall/" + limit + "/" + pagenumber + "/" + modifiedat
+
+        $.getJSON(queryPath, function (resp) {
+            var data = resp.outpatientfootfall;
+            var totalrecords = resp.totalrecords;
+
+            if (!modifiedat) {
+                modifiedat = "2000-01-01"
+            }
+            else {
+                console.log("modifiedat: " + modifiedat);
+            }
+
+            console.log("totalRecords in the collection: " + totalrecords);
             // Iterate over the JSON object
             for (var i = 0, len = data.length; i < len; i++) {
+                var serialno = (limit * (pagenumber-1)) + i;
                 tableData.push({
-                  
-                "VisitID": data[i].visitid,
-                "VISITDATETIME": data[i].startdate,
-                "HOSPITAL": "",
-                "HOSPITALUNIT":"",
-                "MRN": data[i].mrn,
-                "GENDER":"",
-                "Age": "",                               
-                "PATIENTTYPE": "",       
-                "COUNTRY": data[i].country,              
-                "STATE":data[i].state,                  
-                "CITY":data[i].city,
-                "PLACE":"", 
-                "PINCODE": data[i].pincode,
-                "DEPARTMENT": data[i].department,
-                "DOCTOR": data[i].careprovider,
-                "VisitType": "",
-                "APPOINTMENTTYPE":"",
-                "Payor": data[i].payor1,
-                "CREATEDBY": "",
-                "MODIFIEDBY": "",
-                "CREATEDDATETIME": data[i].createdat,
-                "MODIFIEDDATETIME": data[i].modifiedat
-               
+                    "SNo": String(serialno+1),
+                    "regid": data[i]._id,
+                    "id": data[i]._id,
+                    "PatientName": data[i].firstname,
+                    "regdatetime": data[i].registereddate,
+                    "hospital": data[i].hospital,
+                    "hospitalunit": data[i].hospitalunit,
+                    "mrn": data[i].mrn,
+                    "gender": data[i].gender,
+                    "agegroup": "", //To Check
+                    "patienttype": data[i].patienttype,
+                    "country": data[i].country,
+                    "state": data[i].state,
+                    "city": data[i].city,
+                    "pincode": data[i].zipcode,
+                    "latitude": "",
+                    "longitude": "",
+                    "createdat": data[i].createdat,
+                    "createdby": data[i].createdby,
+                    "modifiedby": data[i].modifiedby,
+                    "modifieddatetime": data[i].modifiedat
                 });
             }
-    
-            table.appendRows(tableData);
-            doneCallback();
-        });
-    };
-    
-    myConnector.getSchema = function (schemaCallback) {
-        var cols = [{
-            id: "VisitID",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "VISITDATETIME",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "HOSPITAL",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "HOSPITALUNIT",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id:"MRN",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "GENDER",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "Age",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "COUNTRY",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "STATE",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "CITY",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "PLACE",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "PINCODE",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "DEPARTMENT",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "DOCTOR",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "VisitType",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "APPOINTMENTTYPE",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "Payor",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "CREATEDBY",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "MODIFIEDBY",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id:"CREATEDDATETIME",
-            dataType: tableau.dataTypeEnum.string
-        },{
-            id: "MODIFIEDDATETIME",
-            dataType: tableau.dataTypeEnum.string
-        }
-    ];
-    
-        var tableSchema = {
-            id: "patientFeed",
-            alias: "Patient reports are listed here...........",
-            columns: cols
+
+            var currentrecords = (limit * pagenumber);
+            if (currentrecords < totalrecords) {
+                console.log("Fetching Again with currentrecords: " + currentrecords);
+                pagenumber++;
+                myConnector.getData(table, doneCallback);
+            }
+            else {
+                pagenumber = 1;
+                table.appendRows(tableData);
+                console.log("CompletedRecords: " + tableData.length);
+                tableData = [];
+                doneCallback();
+            }
+            });
         };
-    
-        schemaCallback([tableSchema]);
-    };
-})();
+        
+        myConnector.getSchema = function (schemaCallback) {
+            var cols = [{
+                id: "id",
+                dataType: tableau.dataTypeEnum.string
+            },{
+                id: "SNo",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "PatientName",
+                alias: "Patient Name",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "createdat",
+                alias: "createdat",
+                dataType: tableau.dataTypeEnum.datetime
+            }, {
+                id: "modifieddatetime",
+                alias: "modifieddatetime",
+                dataType: tableau.dataTypeEnum.datetime
+            }, {
+                id: "createdby",
+                alias: "createdby",
+                dataType: tableau.dataTypeEnum.string
+            },
+            {
+                id:"regdatetime",
+                dataType: tableau.dataTypeEnum.datetime
+            }, {
+                id: "country",
+                alias: "country",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "state",
+                alias: "state",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "city",
+                alias: "city",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "pincode",
+                alias: "pincode",
+                dataType: tableau.dataTypeEnum.string
+            }];
+        
+            var tableSchema = {
+                id: "Patients",
+                alias: "Patient reports are listed here...........",
+                columns: cols,
+                incrementColumnId: "modifieddatetime"
+            };
+        
+            schemaCallback([tableSchema]);
+        };
+
+    tableau.registerConnector(myConnector);
+    })();
